60fa17703ddc73ccc9a486d2e8ded88ca9c773fb
sj2901 <saravananjayaraman959@gmail.com>
2020-08-08 19:52:59 +0530

    Co-authored-by: Prasad <srnppmp@yahoo.com> Co-authored-by: hariofficial <haranh845@gmail.com>

--- Account/views.py
+++ Account/views.py
@@ -1,27 +1,46 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
-def login(request):
-    return render(request, 'login.html')
-
-
+def index(request):
+    return render(request,'index.html')
+    
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        phone_num = request.POST['phone']
 
-        user = User.objects.create_user(username=username, password=password,email=email,phone_num=phone_num)
+        # if User.objects.filter(username=username).exists():
+        #     messages.info(request,'Username taken')
+        #     # return redirect('index.html')
+        # elif User.objects.filter(email=email).exists():
+        #     messages.info(request,'Email taken')
+        #     # return redirect('index.html')
+        # else:
+        user = User.objects.create_user(username=username, password=password,email=email)
         user.save()
         print('user created')
         return redirect('login')
+    else:
+        return render(request, 'index.html')
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home.html')
+        else:
+            return redirect('index.html')
 
     else:
-        return render(request, 'register.html')
-
+        return render(request,'index.html')
 
 def homepage(request):
     return render(request, 'home.html')
